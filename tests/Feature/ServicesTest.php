4a0a6bda047e443113d4ca6d83cba46d596e9cb7
Cristian Cosenza <me@cristiancosenza.com>
2022-05-26 17:15:20 +0200

    mock services

--- tests/Feature/ServicesTest.php
+++ tests/Feature/ServicesTest.php
@@ -2,21 +2,47 @@
 
 namespace Tests\Feature;
 
+use App\Services\Geocoding\OpenStreetMap;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class ServicesTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function test_use_openstreetmap_service_with_different_token()
+    {
+        Cache::spy()->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('get')
+            ->andReturn('token-da-test');
+
+        $openStreetMap = new OpenStreetMap($this->faker->word);
+
+        $this->assertEquals(
+            'token-da-test',
+            Cache::get('openstreetmap-token')
+        );
+    }
+
+    public function test_openstreetmap_token_mocked()
+    {
+        Cache::spy()
+            ->shouldAllowMockingProtectedMethods()
+            ->shouldReceive('get')
+            ->with('openstreetmap-token')
+            ->andReturn('token-da-test');
+
+        $openStreetMap = new OpenStreetMap($this->faker->word);
+
+        $this->assertEquals(
+            'token-da-test',
+            Cache::get('openstreetmap-token')
+        );
+
+        $this->assertNull(
+            Cache::get('key-not-mocked')
+        );
     }
 }
